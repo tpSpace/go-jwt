cf4d1a8c7effa38af7d48a6e16d6fd845460e449
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2025-01-31 21:59:55 +0700

    login api && complete jwt

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/tpSpace/go-jwt/initializers"
 	"github.com/tpSpace/go-jwt/models"
 	"golang.org/x/crypto/bcrypt"
@@ -64,10 +67,37 @@ func Login(c *gin.Context) {
 		return
 	}
 	// Look up quested user in the database
+	var user models.User
+
+	initializers.DB.Where("email = ?", body.Email).First(&user)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Email or Password"})
+		return
+	}
 
 	// Compare sent in pass with saved user pass hash
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Email or Password"})
+		return
+	}
 
 	// Generate a JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub" : user.ID,
+		"exp" : time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	//  Sign and get the complete encoded token as a string
+	tokenString, err := token.SignedString(os.Getenv("SECRET"))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+		return
+	}
 
 	// Send the token back to the user
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
